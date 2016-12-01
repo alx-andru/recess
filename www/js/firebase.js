500c97d02381b9b03328a6dffd725b4ea0e80c92
Alex <alexongithub@gmail.com>
2016-12-01 11:59:18 -0500

    removed dummy

--- www/js/firebase.js
+++ www/js/firebase.js
@@ -1,10 +1,10 @@
 // Initialize Firebase
 var config = {
-  apiKey: "AIzaSyCvIHqMGPvvVE4UIhiTn_83TsunPZkLOqE",
-  authDomain: "recess-app-005.firebaseapp.com",
-  databaseURL: "https://recess-app-005.firebaseio.com",
-  storageBucket: "recess-app-005.appspot.com",
-  messagingSenderId: "919318314091"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  storageBucket: "",
+  messagingSenderId: ""
 };
 
 firebase.initializeApp(config);
